f3322d86ba927a849e1e0b56b544f390307e0b3e
findryDev <72649730+findryDev@users.noreply.github.com>
2021-06-16 23:52:20 +0200

    add first class: filed and game object

--- main.py
+++ main.py
@@ -1,18 +1,44 @@
+from typing import SupportsAbs
 import pygame
 import os
 
-WIDTH, HEIGHT = 900, 500
 
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-FPS = 60
 
+class field:
+    def __init__(self):
+        self.WIDTH = 900
+        self.HEIGHT = 500
+        self.FPS = 60
+
+
+class gameObject(field):
+    def __init__(self, window):
+        super().__init__()
+        self.window = window
+        self.rectWIDTH = self.WIDTH//10
+        self.rectHEIGHT = self.HEIGHT//10
+        self.rectPOSITION = (0, 0, self.rectWIDTH, self.rectHEIGHT)
+        self.rectCOLOR = (255, 255, 0)
+
+    def draw(self):
+        pygame.draw.rect(self.window, self.rectCOLOR, self.rectPOSITION)
+        pygame.display.update()
+
+
+gameFiled = field()
+
+WIN = pygame.display.set_mode((gameFiled.WIDTH, gameFiled.HEIGHT))
+pygame.display.set_caption('PONG')
+
+gameOBJ = gameObject(WIN)
 
 def main():
     clock = pygame.time.Clock()
     run = True
     while run:
-        clock.tick(FPS)
+        clock.tick(gameFiled.FPS)
         for event in pygame.event.get():
+            gameOBJ.draw()
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
